b5df165f184fda7c59a177688e2981033a6cf5d7
Robert Norlander <robert.norlander@gmail.com>
2013-09-05 18:56:35 +0200

    Add update method to WebGameMaker
    The update method should update all subsystems
    that might need to be updated, since this is the
    application main loop.

--- web_game_maker.js
+++ web_game_maker.js
@@ -7,6 +7,17 @@ WebGameMaker.init = function() {
         WebGameMaker.UI.addPluginButton(plugins[p]);
     }
 
+    WebGameMaker.Game = new Game();
+    WebGameMaker.update();
+}
+
+WebGameMaker.update = function() {
+    draw_info = {
+        'canvas_context': document.getElementById('canvas').getContext('2d'),
+    }
+
+    WebGameMaker.Game.redraw(draw_info);
+    window.requestAnimationFrame(WebGameMaker.update);
 }
 
 window.addEventListener('load', function() {
