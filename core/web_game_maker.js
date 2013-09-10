99c224ec8fe22dca507222aac9ac8fcfc1cc55b1
Jesper Gustin <sirjeppe@gmail.com>
2013-09-10 19:39:25 +0200

    Moving plugins_import.js to plugin_manager.js

--- core/web_game_maker.js
+++ core/web_game_maker.js
@@ -11,7 +11,7 @@ WebGameMaker.setup = {
 WebGameMaker.requiredResources = [
     WebGameMaker.setup.coreFolder + '/utils.js',
     WebGameMaker.setup.coreFolder + '/game.js',
-    WebGameMaker.setup.coreFolder + '/plugins_import.js',
+    WebGameMaker.setup.coreFolder + '/plugin_manager.js',
     WebGameMaker.setup.coreFolder + '/ui.js',
 ];
 
